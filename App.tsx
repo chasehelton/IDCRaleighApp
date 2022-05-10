ae484f755933ad1067ed6e013a9714e5959b3fa6
jchelton <jchelton@ncsu.edu>
2022-05-10 09:05:19 -0400

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Videos from './Videos';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Videos />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    overflow: 'hidden',
+    maxHeight: 0,
+    maxWidth: 0,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
